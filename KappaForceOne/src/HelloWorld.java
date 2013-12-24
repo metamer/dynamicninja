48b30fcd438dcb1ff1e0d5e58376a1bfd86a887a
metamer <metamer@openmailbox.org>
2013-12-24 18:46:58 -0500

    testing commit from vm

--- KappaForceOne/src/HelloWorld.java
+++ KappaForceOne/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	//Method to test basic setup of development environment
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"); //testing commit from vm
 	}
 
 }
