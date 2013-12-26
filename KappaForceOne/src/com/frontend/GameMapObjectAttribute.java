99606b7d7d2bcc5dcca93459b73996d304eff3a7
metamer <metamer@openmailbox.org>
2013-12-25 19:10:11 -0500

    Added enums for gamemapobject

---
+++ KappaForceOne/src/com/frontend/GameMapObjectAttribute.java
@@ -0,0 +1,10 @@
+package com.frontend;
+
+/*
+ * Attributes that a GameMapObject can have. Initially based on attributes available in JCurses
+ */
+public enum GameMapObjectAttribute {
+  NORMAL,
+  BOLD,
+  REVERSE
+}
---
+++ KappaForceOne/src/com/frontend/GameMapObjectColor.java
@@ -0,0 +1,15 @@
+package com.frontend;
+
+/*
+ * Possible colors for a GameMapObject. Initially based on available colors in JCurses
+ */
+public enum GameMapObjectColor {
+  BLACK,
+  BLUE,
+  CYAN,
+  GREEN,
+  MAGENTA,
+  RED,
+  YELLOW,
+  WHITE
+}
