f728948380b229a60fa79c141d79ab1b0ae2c26d
metamer <metamer@openmailbox.org>
2013-12-25 19:45:01 -0500

    defined constructors and added enums to gamemapobject

--- KappaForceOne/src/com/frontend/GameMapObject.java
+++ KappaForceOne/src/com/frontend/GameMapObject.java
@@ -1,6 +1,27 @@
 package com.frontend;
 
+/*
+ * Defines elements of a GameMap 
+ */
 public class GameMapObject {
+  
   public char symbol;
+  public GameMapObjectColor color;
+  public GameMapObjectAttribute attribute;
+  
+  public GameMapObject(char symbol, GameMapObjectColor color, GameMapObjectAttribute attribute){
+   this.symbol=symbol;
+   this.color=color;
+   this.attribute=attribute;
+  }
+  
+  /*
+   * copy constructor
+   */
+  public GameMapObject(GameMapObject other){
+   this.symbol=other.symbol;
+   this.color=other.color;
+   this.attribute=other.attribute;
+  }
   
 }
