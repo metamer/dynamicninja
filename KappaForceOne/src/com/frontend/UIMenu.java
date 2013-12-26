b518de039b46e632a2a2f8f59ddf4c2405bc89ff
metamer <metamer@openmailbox.org>
2013-12-26 00:06:57 -0500

    Created UIMenu object and added constructors to UIState

---
+++ KappaForceOne/src/com/frontend/UIMenu.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.frontend;
+
+/**
+ * @author insidiousnoxious
+ * Represents menus available through the UI
+ */
+public class UIMenu {
+  
+  private String name;
+  
+  public UIMenu(String name){
+    this.name=name;
+  }
+  
+  //accessor methods
+  
+  public String getName(){
+    return name;
+  }
+
+}
--- KappaForceOne/src/com/frontend/UIState.java
+++ KappaForceOne/src/com/frontend/UIState.java
@@ -3,10 +3,47 @@
  */
 package com.frontend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author insidiousnoxious
  *
  */
 public class UIState {
+  
+  private List<UIMenu> menuList; 
+  private GameMap gameMap;
+  
+  /*
+   * Initialize with an empty List
+   */
+  public UIState(GameMap gameMap){
+    this.gameMap = gameMap;
+    this.menuList = new ArrayList<UIMenu>();
+  }
+  
+  /*
+   * Initialize with given list and gameMap
+   */
+  public UIState(List<UIMenu> menuList, GameMap gameMap){
+    this.menuList = menuList;
+    this.gameMap = gameMap;
+  }
+  
+  /**
+   * @return the menuList
+   */
+  public List<UIMenu> getMenuList() {
+    return menuList;
+  }
+  /**
+   * @return the gameMap
+   */
+  public GameMap getGameMap() {
+    return gameMap;
+  }
+  
+  
 
 }
