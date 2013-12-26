184f3023fbf1bbf1b5700a0cf3b0ed142901545b
metamer <metamer@openmailbox.org>
2013-12-26 00:46:50 -0500

    Added specs to Runner interface

--- KappaForceOne/src/com/runner/Runner.java
+++ KappaForceOne/src/com/runner/Runner.java
@@ -1,10 +1,29 @@
 package com.runner;
 
+import com.all.*;
 import com.frontend.*;
 import com.backend.*;
 
 public interface Runner {
   
+  /*
+   * Accessor for current UIState
+   */
+  public UIState getUIState();
   
+  /*
+   * Accessor for current GameState
+   */
+  public GameState getGameState();
+  
+  /*
+   * Update GameState based on action
+   */
+  public void UpdateGameState(GameAction gameAction);
+  
+  /*
+   * Map input userAction to GameAction
+   */
+  public GameAction MapUserAction(UserAction inputUserAction);
 
 }
