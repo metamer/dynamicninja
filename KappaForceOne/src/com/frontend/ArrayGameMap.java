32a746276769d3983611f7a994e6a5b65484f081
metamer <metamer@openmailbox.org>
2013-12-29 01:27:22 -0500

    tests for arraygamemap

--- KappaForceOne/src/com/frontend/ArrayGameMap.java
+++ KappaForceOne/src/com/frontend/ArrayGameMap.java
@@ -1,3 +1,6 @@
+/**
+ * Implementation of GameMap using an Array
+ */
 package com.frontend;
 
 public class ArrayGameMap implements GameMap {
@@ -6,16 +9,64 @@ public class ArrayGameMap implements GameMap {
   private int height, width;
   private GameMapEntry defaultGameMapEntry;
   
+  /**
+   * Initialize new ArrayGameMap with all elements null
+   * @param height Height of map
+   * @param width Width of map
+   */
   public ArrayGameMap(int height, int width){
    gameMapArray = new GameMapEntry[height][width];
    this.height = height;
    this.width = width;
   }
   
+  /**
+   * Initialize new ArrayGameMap with default
+   * @param height Height of map
+   * @param width Width of map
+   * @param defaultGameMapEntry GameMapEntry object to default to if none stored at location. If this is null, then no default will be set
+   */
   public ArrayGameMap(int height, int width, GameMapEntry defaultGameMapEntry){
     this(height,width);
     this.defaultGameMapEntry=defaultGameMapEntry;
    }
+
+  /**
+   * Copy constructor for other ArrayGameMaps. Performs deep copy. 
+   * Should be more efficient than GameMap copy constructor due to special handling of defaultGameMapEntry
+   * @param other ArrayGameMap to deep copy
+   */
+  public ArrayGameMap(ArrayGameMap other){
+    this(other.height,other.width);
+    if(other.defaultGameMapEntry != null){
+      this.defaultGameMapEntry = new GameMapEntry(other.defaultGameMapEntry);
+    }
+    
+    for(int row = 0; row < height; row++){
+      for(int column = 0; column < width; column++){
+        //only copy those items where object is not null and default would not be used
+        if(other.getObjectAt(row, column) != null &&  !other.useDefaultGameMapEntryAt(row, column)){
+          this.setObjectAt(row, column, new GameMapEntry(other.getObjectAt(row,column)));
+        }
+      }
+    }
+    
+  }
+  
+  /**
+   * Copy constructor for GameMaps. Performs deep copy.
+   * @param other GameMap to deep copy
+   */
+  public ArrayGameMap(GameMap other){
+    this(other.getHeight(),other.getWidth());
+    for(int row = 0; row < height; row++){
+      for(int column = 0; column < width; column++){
+        if(other.getObjectAt(row,column) != null){
+        this.setObjectAt(row, column, new GameMapEntry(other.getObjectAt(row,column)));
+        }
+      }
+    }
+  }
   
   @Override
   public int getHeight() {
@@ -30,8 +81,8 @@ public class ArrayGameMap implements GameMap {
   @Override
   public GameMapEntry getObjectAt(int row, int column) {
     
-    //if defaultGameMapEntry is defined and gamemaparray is null, return default
-    if(defaultGameMapEntry != null && gameMapArray[row][column] == null ){
+
+    if(useDefaultGameMapEntryAt(row,column)){
       return defaultGameMapEntry;
     }
     
@@ -42,6 +93,51 @@ public class ArrayGameMap implements GameMap {
   public void setObjectAt(int row, int column, GameMapEntry entryToSet) {
     gameMapArray[row][column] = entryToSet;
     
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+        return false;
+    }
+    if (getClass() != obj.getClass()) {
+        return false;
+    }
+    final ArrayGameMap other = (ArrayGameMap) obj;
+  
+    if (this.getHeight() != other.getHeight()) {
+        return false;
+    }
+
+    if (this.getWidth() != other.getWidth()) {
+        return false;
+    }
+    
+    if ((this.defaultGameMapEntry == null) ? (other.defaultGameMapEntry != null) : !this.defaultGameMapEntry.equals(other.defaultGameMapEntry)) {
+      return false;
+    }
+    
+    
+    for(int row = 0; row < this.height ; row++){
+      for (int column = 0; column< this.width ; column++){
+        if ((this.getObjectAt(row, column) == null) ? (other.getObjectAt(row, column) != null) : !this.getObjectAt(row, column).equals(other.getObjectAt(row, column))) {
+          return false;
+        }
+      }
+    }
+    
+    return true;
+  }
+  
+  /**
+   * Determine if defaultGameMapEntry should be returned for the input coordinates
+   * @param row 
+   * @param column
+   * @return true if object returned is defaultGameMapEntry
+   */
+  private boolean useDefaultGameMapEntryAt(int row, int column){
+    //if defaultGameMapEntry is defined and gameMapArray is null, use default
+    return ( defaultGameMapEntry != null && gameMapArray[row][column] == null );
   }
 
 }
--- KappaForceOne/src/test/com/frontend/ArrayGameMapTest.java
+++ KappaForceOne/src/test/com/frontend/ArrayGameMapTest.java
@@ -5,7 +5,14 @@ package test.com.frontend;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.frontend.ArrayGameMap;
+import com.frontend.GameMap;
+import com.frontend.GameMapEntry;
+import com.frontend.GameMapEntryAttribute;
+import com.frontend.GameMapEntryColor;
 
 /**
  * @author insidiousnoxious
@@ -13,9 +20,84 @@ import org.junit.Test;
  */
 public class ArrayGameMapTest {
 
+  private ArrayGameMap agm, agmd, agmNull;
+  private GameMapEntry gme;
+  
+  @Before
+  public void setUp(){
+    gme = new GameMapEntry('x',GameMapEntryColor.BLUE, GameMapEntryColor.BLACK, GameMapEntryAttribute.BOLD);
+    agm = new ArrayGameMap(15,44);
+    agmd = new ArrayGameMap(15,44,gme);
+    agmNull = null;
+  }
+  
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testEqualsWithoutDefault() {
+    ArrayGameMap agm2 = new ArrayGameMap(15,44);
+    ArrayGameMap agm3 = new ArrayGameMap(15,44,null);
+    ArrayGameMap agm4 = new ArrayGameMap(15,10);
+    ArrayGameMap agm5 = new ArrayGameMap(50,44);
+    
+    assertEquals(agm, agm2);
+    assertEquals(agm, agm3);
+    assertNotEquals(agm, agm4);
+    assertNotEquals(agm, agm5);
+    assertNotEquals(agm, agmd);
+    
+  }
+  
+  @Test
+  public void testEqualsWithDefault() {
+    ArrayGameMap agm2 = new ArrayGameMap(15,44,gme);
+    ArrayGameMap agm3 = new ArrayGameMap(15,44,null);
+    ArrayGameMap agm4 = new ArrayGameMap(15,10,gme);
+    ArrayGameMap agm5 = new ArrayGameMap(50,44,gme);
+    
+    assertEquals(agmd, agm2);
+    assertNotEquals(agmd, agm3);
+    assertNotEquals(agmd, agm4);
+    assertNotEquals(agmd, agm5);
+    assertNotEquals(agmd, agm);
+    
+  }
+  
+  @Test
+  public void testEqualsSpecialCasesWithoutDefault(){
+    assertEquals(agm,agm);
+    assertNotEquals(agm,agmNull);
+    
+  }
+  
+  @Test
+  public void testEqualsSpecialCasesWithDefault(){
+    assertEquals(agmd,agmd);
+    assertNotEquals(agmd,agmNull);
+  }
+  
+  @Test
+  public void testCopyConstructorForInterface(){
+    ArrayGameMap agmCopy = new ArrayGameMap((GameMap)agm);
+    assertEquals(agm, agmCopy);
+    assertNotSame(agm,agmCopy);
+    
+    //copied map will not have default set
+    ArrayGameMap agmdCopy = new ArrayGameMap((GameMap)agmd);
+    assertNotEquals(agmd, agmdCopy);
+    assertNotEquals(agm, agmdCopy);
+  }
+  
+  @Test
+  public void testCopyConstructorForClass(){
+    ArrayGameMap agmCopy = new ArrayGameMap(agm);
+    assertEquals(agm, agmCopy);
+    assertNotSame(agm, agmCopy);
+    
+    //copied map will have default set
+    ArrayGameMap agmdCopy = new ArrayGameMap(agmd);
+    assertEquals(agmd, agmdCopy);
+    assertNotSame(agmd, agmdCopy);
+    
+    assertNotEquals(agm, agmdCopy);
   }
 
 }
