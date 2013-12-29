ed2ed9634c137d65939df26dc6be834507089c0c
metamer <metamer@openmailbox.org>
2013-12-29 01:48:11 -0500

    added skeleton test classes for ui and runner interfaces

---
+++ KappaForceOne/src/test/com/runner/RunnerTest.java
@@ -0,0 +1,14 @@
+package test.com.runner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RunnerTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ KappaForceOne/src/test/com/ui/UITest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test.com.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * 
+ * @author insidiousnoxious
+ *
+ */
+public class UITest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
